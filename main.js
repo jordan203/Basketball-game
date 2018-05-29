f08f507fa505880e4d353d422b8dbed6596d802b
Jordan 203 <jayjaysmith.7217@gmail.com>
2018-05-29 13:59:23 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var age=89	;
+var name="jordan";
+
+if (age>18 && age <66)
+
+{
+	alert("you are an adult");
+}
+else if (age>68)
+{
+	alert (" you got a walking stick");
+}
+else 
+{
+	alert(" you are not an adult");
+}
