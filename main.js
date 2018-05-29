3c63c2ed5ca073e0ade2e8e5c4be99db0005bfa8
Jordan 203 <jayjaysmith.7217@gmail.com>
2018-05-29 15:54:00 +1000

    setup play information

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-var age=89	;
-var name="jordan";
+var player_name;
 
-if (age>18 && age <66)
+function Player(name)
+{
+	this.name = name;
+	this.points =0;
+	this.rebounds =0;
+	this.assists =0;
+	this.blocks =0;
+	this.steals =0;
+	this.player_field_gold_p =0;
+	this.player_3_point_p =0;
 
-{
-	alert("you are an adult");
+
 }
-else if (age>68)
+
+var get_player_name = function()
 {
-	alert (" you got a walking stick");
+	player_name = document.getElementById("name-input").value;
+
+	var name_container = document.getElementById("name-enter");
+	name_container.style.visibility = "hidden";
 }
-else 
+
+var hide_element = function(id)
 {
-	alert(" you are not an adult");
+	var ele = document.getElementById(id);
+	ele.style.visibility="hiden";
+
+}
+
+var show_element = function(id)
+{
+	var ele = document.getElementById(id);
+	ele.style.visibility = "visible";
 }
