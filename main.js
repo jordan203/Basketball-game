9c15b4fc24d0681a310f5a01aeb451f0b1f4bfd0
Brandyn <Brandynbb96@gmail.com>
2018-06-12 15:55:08 +1000

    Added team display and comments to the code

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 var player_name;
-var team;
+var player_team, cpu_team;
 
+
+
+// Template for our team
+function Team(name)
+{
+	this.name = name;
+	this.player_count = 15;
+	this.players = [];
+}
+
+// Template for our players
 function Player(name)
 {
 	this.name = name;
@@ -12,12 +23,6 @@ function Player(name)
 	this.player_field_gold_p = 0;
 	this.player_3_point_p = 0;
 
-}
-function Team(name)
-{
-	this.name = name;
-	this.player_count = 15;
-	this.players = [];
 }
 
 var player_html = "" + 
@@ -34,27 +39,41 @@ var player_html = "" +
 
 var get_player_name = function()
 {
+	// Get the name of the player
 	player_name = document.getElementById("name-input").value;
 
+	// Hide the name setting of teh player
 	var name_container = document.getElementById("name-enter");
 	name_container.style.visibility = "hidden";
 
-	setup_team();
+	// Create player team and display it on the layout
+	player_team = setup_team(player_name);
+	display_team(player_team, true);
+
+	// Create the CPU team and display it on the layout
+	cpu_team = setup_team("Computer");
+	display_team(cpu_team, false);
 }
 
-//: New Function
-var setup_team = function()
+var setup_team = function(team_name)
 {
-	team = new Team(player_name);
+	// Create a new team using the template
+	var team = new Team(team_name);
+
+	// Create a new player list and add it to the teams
 	for (var ii = 0; ii < team.player_count; ii += 1)
 	{
-		var name="Player-" + ii;
+		var name = "Player-" + ii; // For testing
 		team.players[ii] = new Player(name);  
 	} 
+
+	// Return the new team to the variable outside the function
+	return team;
 }
 
 var hide_element = function(id)
 {
+	// Hide the element
 	var ele = document.getElementById(id);
 	ele.style.visibility="hiden";
 
@@ -62,6 +81,42 @@ var hide_element = function(id)
 
 var show_element = function(id)
 {
+	// Display the element
 	var ele = document.getElementById(id);
 	ele.style.visibility = "visible";
+}
+
+var display_team = function(team, is_player)
+{
+	// Get the container for the team
+	var ele;
+
+	// Do we need the player container of cpu container?
+	if (is_player)
+	{
+		// Get the player container
+		ele = document.getElementById("player-container");
+	} else {
+		// Get the cpu container
+		ele = document.getElementById("cpu-container");		
+	}
+
+	// Get the team list from the html
+	ele = ele.getElementsByClassName('team')[0];
+
+	// Reset the list so we don't keep adding by mistake
+	ele.innerHTML = "";
+
+	// Display the players in the box
+	for (var i = 0; i < team.player_count; i ++)
+	{
+		// Get the player
+		var player = team.players[i];
+
+		// Create a new HTML list
+		var player_html = "<li>" + player.name + "</li>";
+
+		// Add the new player to the layout 
+		ele.innerHTML += (player_html);
+	}
 }
